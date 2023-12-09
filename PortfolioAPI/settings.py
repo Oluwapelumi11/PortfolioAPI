39e4ca76bf2bc42a6952b67088ff008d490ed3c2
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2023-12-09 11:24:04 +0100

    required files

--- PortfolioAPI/settings.py
+++ PortfolioAPI/settings.py
@@ -137,9 +137,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-
-STATICFILES_DIRS = (STATIC_DIR,)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "media/"
 # Default primary key field type
