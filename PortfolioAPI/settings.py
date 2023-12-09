6ed901535738b09b0c9127870badfbf12a4a5346
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2023-12-09 11:12:01 +0100

    test

--- PortfolioAPI/settings.py
+++ PortfolioAPI/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (STATIC_DIR,)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR/"media")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
