d03fbebc03a7bd1b0619ce328e17ccea45121027
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-01-31 22:20:32 +0100

    adedddd

--- qrcreator/views.py
+++ qrcreator/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from .serialize import QrCodeSerializer, QrCodeSchema
+from qrcode_generator.serializers import QrCodeSerializer, QrCodeSchema
 import qrcode
+from io import BytesIO
 
 class GenerateQrCode(APIView):
     def get(self, request, *args, **kwargs):
@@ -26,7 +27,13 @@ class GenerateQrCode(APIView):
 
         img = qr.make_image(fill_color=qr_params['fill_color'], back_color=qr_params['back_color'])
 
-        # Serve the image directly
-        response = Response(content_type="image/png")
-        img.save(response, format="PNG")
+        # Save the image to a BytesIO buffer
+        image_buffer = BytesIO()
+        img.save(image_buffer, format="PNG")
+        image_data = image_buffer.getvalue()
+
+        # Set HTTP headers for download
+        response = HttpResponse(image_data, content_type="image/png")
+        response['Content-Disposition'] = 'attachment; filename="qrcode.png"'
+
         return response
