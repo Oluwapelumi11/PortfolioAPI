e590952c5497b162246a10fc9a41471742e1508a
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-01-31 22:38:41 +0100

    a

--- qrcreator/serialize.py
+++ qrcreator/serialize.py
@@ -3,10 +3,10 @@ from marshmallow import Schema, fields, validate
 
 class QrCodeSerializer(serializers.Serializer):
     data = serializers.CharField(required=True)
-    fill_color = serializers.CharField(default="black")
-    back_color = serializers.CharField(default="white")
+    fill = serializers.CharField(default="black")
+    back = serializers.CharField(default="white")
 
 class QrCodeSchema(Schema):
     data = fields.String(required=True, validate=validate.Length(min=1))
-    fill_color = fields.String(missing="black")
-    back_color = fields.String(missing="white")
+    fill = fields.String(missing="black")
+    back = fields.String(missing="white")
--- qrcreator/views.py
+++ qrcreator/views.py
@@ -7,12 +7,11 @@ from io import BytesIO
 
 class GenerateQrCode(APIView):
     def get(self, request, *args, **kwargs):
-        # Deserialize input from the request
         serializer = QrCodeSerializer(data=request.query_params)
         serializer.is_valid(raise_exception=True)
         params = serializer.validated_data
 
-        # Generate QR code using the provided or default values
+   
         qr_schema = QrCodeSchema()
         qr_params = qr_schema.load(params)
 
@@ -25,15 +24,15 @@ class GenerateQrCode(APIView):
         qr.add_data(qr_params['data'])
         qr.make(fit=True)
 
-        img = qr.make_image(fill_color=qr_params['fill_color'], back_color=qr_params['back_color'])
+        img = qr.make_image(fill_color=qr_params['fill'], back_color=qr_params['back'])
 
-        # Save the image to a BytesIO buffer
+   
         image_buffer = BytesIO()
         img.save(image_buffer, format="PNG")
         image_data = image_buffer.getvalue()
 
-        # Set HTTP headers for download
+        
         response = HttpResponse(image_data, content_type="image/png")
-        response['Content-Disposition'] = 'attachment; filename="qrcode.png"'
+        response['Content-Disposition'] = 'attachment; filename="myqr.png"'
 
         return response
